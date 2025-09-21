b3f91a9cafc42dbe3c8a733c2a3e6d42cd695c5d
kassandra <kassandra@localhost.localdomain>
2025-09-21 11:29:26 +0300

    Added some checking

--- main.c
+++ main.c
@@ -9,38 +9,48 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
-typedef enum {
-	ARG_BOOL_FLAG,
-	ARG_ONE_FLAG,
-	ARG_MANY_FLAGS
-} ArgType;
-
-
 typedef struct {
     char** items;
     size_t count;
     size_t capacity;
 } ArrayOfStrings;
 
+
 typedef union {
 	bool           exists;    // used for ARG_BOOL_FLAG
-	char*    one_data;  // used for ARG_ONE_FLAG (pointer to single string)
+	char*          one_data;  // used for ARG_ONE_FLAG (pointer to single string)
 	ArrayOfStrings many_data; // used for ARG_MANY_FLAGS (pointer to pointer of single strings)
 } ArgData;
 
 
+typedef enum {
+	ARG_BOOL_FLAG,
+	ARG_ONE_FLAG,
+	ARG_MANY_FLAGS
+} ArgType;
+
+const char * argTypeToString(ArgType a)
+{
+	if (a == ARG_BOOL_FLAG) return "ARG_BOOL_FLAG";
+	if (a == ARG_ONE_FLAG)  return "ARG_ONE_FLAG";
+	if (a == ARG_MANY_FLAGS) return "ARG_MANY_FLAGS";
+	nob_log(ERROR, "Unknown argument type %s", a);
+	abort();
+}
+
 typedef struct {
-	ArgType    type;
+	ArgType     type;
 	const char* flag;
-	ArgData    data;
+	ArgData     data;
+	bool        required;
 } Arg;
+
 
 typedef struct {
        Arg*      items;
        size_t    count;
        size_t    capacity;
 } Args;
-
 
 
 typedef enum {
@@ -53,6 +63,7 @@ typedef struct {
 	ArgTokenType type;
 	const char*  data;
 } ArgToken;
+
 
 typedef struct {
        ArgToken* items;
@@ -178,52 +189,104 @@ bool parseBoolFlag(const ArgTokens* tokens, size_t* current_count, Args* args_pa
 bool parseArguments(const ArgTokens* tokens, Args* args_parsed)
 {
 	size_t current_count = 0;
-	nob_log(INFO, "tokens count %d", tokens->count);
 	while (current_count < tokens->count) {
-		nob_log(INFO, "current count out %d", current_count);
 		if (!parseManyFlags(tokens, &current_count, args_parsed)) continue;
 		if (!parseOneFlag(tokens, &current_count, args_parsed))   continue;
 		if (!parseBoolFlag(tokens, &current_count, args_parsed))  continue;
-	 	//size_t index = t - tokens.items;
-		//nob_log(INFO, "%d, %s", t->type, t->data);
 	}
 
 }
 
 
-int main(int argc, char** argv)
+bool checkParsedArguments(const Args* args_parsed)
 {
-	Arg args[] = {
-		[0].type = ARG_BOOL_FLAG,
-		[0].flag = "--test",
+	// Duplicate flags check
+	da_foreach(Arg, a, args_parsed) {
+		da_foreach(Arg, b, args_parsed) {
+			if (a != b && !strcmp(b->flag,a->flag)) {
+				nob_log(ERROR, "Duplicate flags %s provided", a->flag);
+				abort();
+			}
+		}
+	}
+}
 
-		[1].type = ARG_ONE_FLAG,
-		[1].flag = "--output",
-		 
-		[2].type = ARG_MANY_FLAGS,
-		[2].flag = "--inputs"
-	};
+
+bool argParse(int argc, char** argv, Arg* args, size_t args_size)
+{
 
 	ArgTokens tokens = {0};
 	tokenizeArguments(argc, argv, &tokens);
-	da_foreach(ArgToken, t, &tokens) {
-	       	size_t index = t - tokens.items;
-		nob_log(INFO, "%d, %s", t->type, t->data);
-	}
+
+	//da_foreach(ArgToken, t, &tokens) {
+	//     	size_t index = t - tokens.items;
+	//	nob_log(INFO, "%d, %s", t->type, t->data);
+	//}
 
 	Args args_parsed = {0};
 	parseArguments(&tokens, &args_parsed);
+	checkParsedArguments(&args_parsed);
 
-	da_foreach(Arg, a, &args_parsed) {
-		nob_log(INFO, "flag: %s", a->flag);
-		if (a->type == ARG_MANY_FLAGS) {
-			da_foreach(char*, str, &a->data.many_data) {
-				nob_log(INFO, "many data: %s", *str);
+	for (size_t i = 0; i < args_size; i++) {
+		// Find parsed arg
+		Arg* arg_parsed;
+		bool found = false;
+		da_foreach(Arg, a, &args_parsed) {
+			if (!strcmp(args[i].flag, a->flag)) {
+				arg_parsed = a;
+				found = true;
+				break;
 			}
-		} else if (a->type == ARG_ONE_FLAG) {
-				nob_log(INFO, "one data: %s", a->data.one_data);
-		} else if (a->type == ARG_BOOL_FLAG) {
-				nob_log(INFO, "bool flag: %d", a->data.exists);
+		}
+
+		if (!found && args[i].required) {
+			nob_log(ERROR, "Input argument %s is required and it is not provided", args[i].flag);
+			abort();
+		} else if (!found && !args[i].required) {
+			continue;
+		}
+		
+		if (args[i].type != arg_parsed->type) {
+			nob_log(ERROR, "Declared argument with flag %s and type %s does not match the parsed one with flag %s and %s",
+					args[i].flag,
+					argTypeToString(args[i].type),
+					arg_parsed->flag,
+					argTypeToString(arg_parsed->type));
+			abort();
 		}
 	}
+
+	//da_foreach(Arg, a, &args_parsed) {
+	//	nob_log(INFO, "flag: %s", a->flag);
+	//	if (a->type == ARG_MANY_FLAGS) {
+	//		da_foreach(char*, str, &a->data.many_data) {
+	//			nob_log(INFO, "many data: %s", *str);
+	//		}
+	//	} else if (a->type == ARG_ONE_FLAG) {
+	//			nob_log(INFO, "one data: %s", a->data.one_data);
+	//	} else if (a->type == ARG_BOOL_FLAG) {
+	//			nob_log(INFO, "bool flag: %d", a->data.exists);
+	//	}
+	//}
+}
+
+
+
+int main(int argc, char** argv)
+{
+	Arg args[] = {
+		[0].type     = ARG_BOOL_FLAG,
+		[0].flag     = "--test",
+		[0].required = true,
+
+		[1].type     = ARG_ONE_FLAG,
+		[1].flag     = "--output",
+		[1].required = true,
+		 
+		[2].type = ARG_MANY_FLAGS,
+		[2].flag = "--inputs",
+		[2].required = true
+	};
+
+	argParse(argc, argv, args, sizeof(args)/sizeof(args[0]));
 }
